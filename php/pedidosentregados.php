e98267909e6cbae9bf51d560386862dd53566c57
chernandez911 <ing.cristianh@gmail.com>
2014-03-08 12:52:35 -0500

    Se agrega la consulta con adodb y se elimina consulta con mysql_query

--- php/pedidosentregados.php
+++ php/pedidosentregados.php
@@ -1,11 +1,9 @@
 <?php 
 include ("../config/config.php");
 	
-$consulta=mysql_query("UPDATE pedidos SET estado=1 WHERE id='".$_GET['id']."'");
-		
-mysql_close($conexion);
+$consulta=$conn->Execute("UPDATE pedidos SET estado=1 WHERE id='".$_GET['id']."'");	
+$consulta->moveNext();
 ?>
 <script>
-
 window.location = "../admin/pedidos.php";
 </script>

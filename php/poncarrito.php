5b2b1472a3424cfd3c15b14a323ccbcf5fb58cf1
chernandez911 <ing.cristianh@gmail.com>
2014-03-08 12:55:32 -0500

    Se cambian las consultas para que queden generales sin depender el motor de base de datos que se usan, esta pagina agrega los item que el usuario selecciono y se ejecuta tambien con el archivo codigo.js que pasa la variable p por get y se ejecuta esta pagina despues

--- php/poncarrito.php
+++ php/poncarrito.php
@@ -1,27 +1,23 @@
+<?php include ("../config/config.php"); ?>
 <?php 
-
  session_start();
  $suma=0;
  if(isset($_GET['p'])){
  $_SESSION['producto'][$_SESSION['contador']]=$_GET['p'];
-  $_SESSION['unidades'][$_SESSION['contador']]=$_GET['cant'];
+ $_SESSION['unidades'][$_SESSION['contador']]=$_GET['cant'];
  $_SESSION['contador']++;
  }
-  
-include ("../config/config.php");
-
 for($i=0;$i<($_SESSION['contador']);$i++)
 {
-	$consulta=mysql_query("select *from productos where id=".$_SESSION['producto'][$i]."");
-
-while($fila=mysql_fetch_array($consulta))
-	{	
-	echo("<table class='table'>");
-	echo "<tr><td>". $_SESSION['unidades'][$i]."</td><td>".$fila['nombre']."</td><td>". ($_SESSION['unidades'][$i]*$fila['precio'])."</td></tr>";
-	$suma+=$fila['precio'] *  $_SESSION['unidades'][$i];
-	}
+	$consulta=$conn->Execute("SELECT *FROM productos WHERE id=".$_SESSION['producto'][$i]."");
+		while(!$consulta->EOF)
+		{
+			echo("<table class='table'>");
+			echo "<tr><td>". $_SESSION['unidades'][$i]."</td><td>".$consulta->fields['nombre']."</td><td>". ($_SESSION['unidades'][$i]*$consulta->fields['precio'])."</td></tr>";
+			$suma+=$consulta->fields['precio'] *  $_SESSION['unidades'][$i];
+			$consulta->moveNext();
+		}
 }
 echo("<tr class='success'><td></td><td>Subtotal:</td><td>".$suma."</tr>");
 echo("</table>");
- mysql_close($conexion);
 ?>

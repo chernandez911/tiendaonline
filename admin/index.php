2a3d3b695c38bca46966c3fcf5c078415f5b825d
chernandez911 <ing.cristianh@gmail.com>
2014-03-10 08:53:37 -0500

    Index del administrador, se creo una session y si la variable de session esta en false se redirige a la pagina de login para que el usuario inicie sesion

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,21 @@
 <?php include("cabecera.php");		?>
-
+<?php 
+session_start();
+if($_SESSION['activo']==false){
+header("Location:../login.php");
+} 
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+	<form action="../php/logout.php" metho="post">
+	<input type="submit" value="logout" name="logout">
+</form>
+</body>
+</html>
 <?php include("piedepagina.php");		?>

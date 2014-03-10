c260e6b56f20576e41c4124c11816e0590fb8f20
chernandez911 <ing.cristianh@gmail.com>
2014-03-10 08:55:44 -0500

    Cuando el administrador crea un nuevo cliente, los datos de usuario y contraseña de los usuarios creados se encriptan, enviandolos encriptados a la base de datos, esto para una mayor seguridad y para que el administrador no conozca el nombre de usuario y contraseña de sus clientes

--- admin/nuevocliente.php
+++ admin/nuevocliente.php
@@ -2,12 +2,11 @@
 include ("../config/config.php");
 	
 $consulta=mysql_query("insert into clientes values (NULL,'".$_POST['nombre']."','".$_POST['apellidos']."','".$_POST['email']."'
-	,'".$_POST['usuario']."','".$_POST['contrasena']."','".$_POST['telefono']."','".$_POST['celular']."','".$_POST['fax']."'
+	,SHA('".$_POST['usuario']."'),SHA('".$_POST['contrasena']."'),'".$_POST['telefono']."','".$_POST['celular']."','".$_POST['fax']."'
 	,'".$_POST['direccion']."','')");
 
 mysql_close($conexion);
 ?>
-
 <script type="text/javascript">
 window.location="clientes.php";
 </script>

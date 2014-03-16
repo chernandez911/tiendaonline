831ef047f71dcf909e10cc76a584c257b2bfd9c2
chernandez911 <ing.cristianh@gmail.com>
2014-03-16 14:58:06 -0500

    Error corregido, no se mostraban los paneles

--- index2.php
+++ index2.php
@@ -7,17 +7,15 @@
 <div  class="container">
 
 <?php
-$consulta=$conn->Execute("SELECT *FROM productos WHERE destacado='1' && existencias>0 LIMIT 10");
+
+$consulta=$conn->Execute("SELECT *FROM productos WHERE destacado='1' && existencias>0 LIMIT 11");
 while (!$consulta->EOF){
 
-$consulta2=$conn->Execute("SELECT * FROM imagenesproductos WHERE id_producto='".$consulta->fields['id']."' LIMIT 1;");	
+$consulta2=$conn->Execute("SELECT * FROM imagenesproductos WHERE id_producto='".$consulta->fields['id']."' LIMIT 1;");
 
-while(!$consulta2->EOF){
-	echo"<div id='productos' class='paneldestacados panel-info col-xs-12 col-sm-6 col-md-6 col-lg-6'> ";
-	echo" <div class='panel-heading'><h><a href='producto.php?id=".$consulta->fields['id']."'>".$consulta->fields['nombre']."</a></h></div> ";
+echo('<div id="productos" class="paneldestacados panel-info col-xs-12 col-sm-6 col-md-6 col-lg-6"> ');
+	echo" <div class='panel-heading '><h><a href='producto.php?id=".$consulta->fields['id']."'>".$consulta->fields['nombre']."</a></h></div> ";
 	echo"<img src='photo/".$consulta2->fields['imagen']."' width=100px class=".'img-rounded'."> "; 
-	$consulta2->moveNext();
-}
 	echo"<div class='col-xs-4 col-sm-4 col-md-4  col-lg-6'>";
 	echo "<h3></h3>";
 	echo "<p>Precio $".$consulta->fields['precio']."</p>";
@@ -26,7 +24,9 @@ while(!$consulta2->EOF){
 	echo "<button value='".$consulta->fields['id']."'class='botoncompra btn btn-default'> Al carrito </button>";
 	echo"</div>";
 	echo"</div>";
+
 	$consulta->moveNext();
+	$consulta2->moveNext();
 }
 ?>
 </div>

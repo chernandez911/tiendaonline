392fb4cb82372cd3acbe2b506fd39d05537ecc72
chernandez911 <ing.cristianh@gmail.com>
2014-03-16 14:57:43 -0500

    Error corregido no se mostraban los paneles por error en las sentencias y en los while

--- categorias.php
+++ categorias.php
@@ -6,17 +6,18 @@
 <div id="enmarcado" class="container">
 <div  class="container">
 <?php
-$consulta=$conn->Execute("SELECT *FROM productos WHERE id_categoria ='".$_GET['id']."' && existencias >0");
+
+$id=$conn->QSTR($_GET["id"],get_magic_quotes_gpc());
+$consulta=$conn->Execute("SELECT *FROM productos WHERE id_categoria =$id && existencias >0");
 while (!$consulta->EOF){
 
-$consulta2=$conn->Execute("SELECT * FROM `imagenesproductos` WHERE id_producto='".$consulta->fields['id']." ' LIMIT 1;");
-while (!$consulta2->EOF){
+$consulta2=$conn->Execute("SELECT * FROM imagenesproductos WHERE id_producto='".$consulta->fields['id']." ' LIMIT 1;");
 
   echo"<div id='productos' class='paneldestacados panel panel-info col-xs-12 col-sm-6 col-md-6 col-lg-6'> ";
   echo" <div class='panel-heading'>".$consulta->fields['nombre']."</div> ";
   echo"<img src='photo/".$consulta2->fields['imagen']."' width=100px class=".'img-rounded'."> "; 
   $consulta2->moveNext();
-}
+
   echo"<div class='col-xs-6 col-sm-6 col-md-6  col-lg-6'>";
   echo "<h3><a href='producto.php?id=".$consulta->fields['id']."'></a></h3>";
   echo "<p>Precio $".$consulta->fields['precio']."</p>";

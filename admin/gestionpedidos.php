28370f40bd72073f7c03507859fbe151fb90c87b
chernandez911 <ing.cristianh@gmail.com>
2014-03-16 12:30:49 -0500

    Arrgelo para la gestion de productos se muestra el id del pedido y todos los datos de lo que pidio el usuario para su posterior gestion

--- admin/gestionpedidos.php
+++ admin/gestionpedidos.php
@@ -3,18 +3,12 @@
 <?php 
 include ("../config/config.php");
 	
-$consulta=mysql_query("SELECT lineaspedido.id_pedido, productos.nombre, lineaspedido.unidades FROM lineaspedido LEFT JOIN productos ON lineaspedido.id_producto = productos.id WHERE lineaspedido.id_cliente='".$_GET['id']."'");
-
-while($fila=mysql_fetch_array($consulta)){
-
-
-echo "<br> La identificacion de su pedido es: '".$fila['id_pedido']."' El producto que compro fue : '".$fila['nombre']."' <br> Cantidad '".$fila['unidades']."'";
-
-
+$id=$conn->QSTR($_GET["id"],get_magic_quotes_gpc());	
+$consulta=$conn->Execute("SELECT lineaspedido.id_pedido, productos.nombre, lineaspedido.unidades FROM lineaspedido LEFT JOIN productos ON lineaspedido.id_producto = productos.id WHERE lineaspedido.id_cliente=$id");
+while(!$consulta->EOF){
+echo "<br> Identificacion del pedido:".$consulta->fields['id_pedido']." <br> Producto Comprado: ".$consulta->fields['nombre']." <br> Cantidad ".$consulta->fields['unidades']."";
+$consulta->moveNext();
 }
-mysql_close($conexion);
 ?>
 </table>
-
-
 <?php include("piedepagina.php");		?>

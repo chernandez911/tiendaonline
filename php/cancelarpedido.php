109876cfe801ac084643d48187625a2476d040b0
chernandez911 <ing.cristianh@gmail.com>
2014-03-16 12:55:12 -0500

    Se eliminaron estos archivos y se reemplazaron en la carpeta de admin porque estas eran opciones del administrador del sistema

--- php/cancelarpedido.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include ("../config/config.php");
-$consulta=$conn->Execute("UPDATE pedidos SET estado=2 WHERE id='".$_GET['id']."'");	
-$consulta->moveNext();
-?>
-<script>
-window.location = "../admin/pedidos.php";
-</script>
--- php/pedidosentregados.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include ("../config/config.php");
-	
-$consulta=$conn->Execute("UPDATE pedidos SET estado=1 WHERE id='".$_GET['id']."'");	
-$consulta->moveNext();
-?>
-<script>
-window.location = "../admin/pedidos.php";
-</script>

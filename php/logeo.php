dbdd2c3bb7597310dd84796418617f83586ba9b9
chernandez911 <ing.cristianh@gmail.com>
2014-03-19 17:14:34 -0500

    Realizando qstr

--- php/logeo.php
+++ php/logeo.php
@@ -4,11 +4,8 @@ session_start();
 
 include "../config/config.php";
 
-
-
-$usuario =$conn->real_escape_string($_POST['user'], get_magic_quotes_gpc ());
-$contrasena =$conn->real_escape_string($_POST['pass'], get_magic_quotes_gpc ());
-
+$usuario =$conn->qstr($_POST['user'], get_magic_quotes_gpc ());
+$contrasena =$conn->qstr($_POST['pass'], get_magic_quotes_gpc ());
 
 $consulta=$conn->Execute("SELECT *FROM admin WHERE user=$usuario AND pass=$contrasena");
 if($consulta->fields['user']==$usuario && $consulta->fields['pass']== $contrasena)

bda2052fda276bdf4c6600264b62e3d1fe74f8d5
chernandez911 <ing.cristianh@gmail.com>
2014-04-03 12:16:05 -0500

    Arreglos en el panel de administracion

--- admin/index.php
+++ admin/index.php
@@ -5,17 +5,4 @@ if($_SESSION['activo']==false){
 header("Location:../admin/login.php");
 } 
 ?>
-<!doctype html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Panel de Control- Administrador</title>
-</head>
-<body>
-	
-	<form action="../admin/logout.php" metho="post">
-	<input type="submit" value="logout" name="logout">
-</form>
-</body>
-</html>
 <?php include("piedepagina.php");?>

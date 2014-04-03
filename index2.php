5945f96808e9ed88f9806b9d0e05d021922c4ce1
chernandez911 <ing.cristianh@gmail.com>
2014-04-03 11:55:24 -0500

    Cambio de panel para mejorar la visualizacion de los productos

--- index2.php
+++ index2.php
@@ -1,36 +1,36 @@
 <?php include"php/cabecera.php" ?>
 <!doctype html>
-<html lang="en">
-<body>
-	<div id="contenedor">
-<div id="enmarcado" class="container">
+
+<div id="contenedor">
 <div  class="container">
-
 <?php
-
 $consulta=$conn->Execute("SELECT *FROM productos WHERE destacado='1' && existencias>0 LIMIT 11");
 while (!$consulta->EOF){
-
 $consulta2=$conn->Execute("SELECT * FROM imagenesproductos WHERE id_producto='".$consulta->fields['id']."' LIMIT 1;");
 
-echo('<div id="productos" class="paneldestacados panel-info col-xs-12 col-sm-6 col-md-6 col-lg-6"> ');
-	echo" <div class='panel-heading '><h><a href='producto.php?id=".$consulta->fields['id']."'>".$consulta->fields['nombre']."</a></h></div> ";
-	echo"<img src='photo/".$consulta2->fields['imagen']."' width=100px class=".'img-rounded'."> "; 
-	echo"<div class='col-xs-4 col-sm-4 col-md-4  col-lg-6'>";
-	echo "<h3></h3>";
 	$numero = $consulta->fields['precio']; 
-	echo "<p>Precio \$ ".number_format($numero,0,",",".")." </p> ";
-	echo"Cantidad:<input type='number' class='btn btn-default' value='1' max='10' min='1' id='numero".$consulta->fields['id']."'>";  
-	echo "<a href='producto.php?id=".$consulta->fields['id']."'><button class='btn btn-ifno'> Mas informacion </button></a>";
-	echo "<button value='".$consulta->fields['id']."'class='botoncompra btn btn-default'> Al carrito </button>";
-	echo"</div>";
-	echo"</div>";
+
+echo('<div id="productos" class="row col-xs-12 col-sm-6 col-md-4 col-lg-4">
+  <div class="paneldestacados col-xs-12 col-sm-6 col-md-4 col-lg-4">');
+  echo'
+    <div class="thumbnail">
+   <center> <h3>Producto Destacado</h3> </center>
+     <img src="photo/'.$consulta2->fields['imagen'].'" width=150px class="img-rounded">
+      <div class="caption">
+        <h3><a href="producto.php?id='.$consulta->fields['id'].'">'.$consulta->fields['nombre'].'</a></h3>
+        <p>Precio $ '.number_format($numero,0,",",".").' </p>
+        <p><a href="producto.php?id='.$consulta->fields['id'].'"><button class="btn btn-ifno"> Ver producto </button></a></p>';
+        echo"Cantidad:<input type='number' class='btn btn-default' value='1' max='10' min='1' id='numero".$consulta->fields['id']."'>";
+    echo('    <button value="'.$consulta->fields['id'].'"class="botoncompra btn btn-default"> Al carrito </button>
+      </div>
+    </div>
+  </div>
+</div>');
 
 	$consulta->moveNext();
 	$consulta2->moveNext();
 }
 ?>
-</div>
 </div>
 </div>
 </body>

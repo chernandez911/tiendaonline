7bde6d1450771a7a343675e61b24fe2409130fd8
chernandez911 <ing.cristianh@gmail.com>
2014-04-03 12:15:29 -0500

    Arreglos en el panel de administracion

--- admin/clientes.php
+++ admin/clientes.php
@@ -1,54 +1,15 @@
 <?php include("cabecera.php");		?>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-<br>
-<table>
-<tr>
-    <td>Nombres</td>
-    <td>Apellidos</td>
-    <td>E-mail</td>
-    <td>Telefono</td>
-    <td>Celular</td>
-    <td>Direccion</td>
-</tr>
-<?php 
-include ("../config/config.php");
-$consulta=$conn->Execute("SELECT *from clientes");
-while(!$consulta->EOF){
 
-	echo "<tr><td>".$consulta->fields['nombre']."</td>
-    <td>".$consulta->fields['apellidos']."</td>
-    <td>".$consulta->fields['email']."</td>
-    <td>".$consulta->fields['telefono']."</td>
-    <td>".$consulta->fields['celular']."</td>
-    <td>".$consulta->fields['fax']."</td>
-    <td>".$consulta->fields['direccion']."</td>
-    <td><a href='actualizarcliente.php?id=".$consulta->fields['id']."''><button>Actualizar Cliente</button> </a></td>
-    <td><a href='eliminarcliente.php?id=".$consulta->fields['id']."''><button>Eliminar Cliente</button> </a></td>
-    </tr>";
-  $consulta->moveNext();
-}
-  
-?>
-</table>
-<br>
-<table>
-    <tr>
-        <form action="nuevocliente.php" method="post">
-        <td><input type="text" name="nombre" placeholder="Introduce Nombre" required="required"/>      </td>
-        <td><input type="text" name="apellidos" placeholder="Introduce Apellidos" required="required" />      </td>
-        <td><input type="email" name="email" placeholder="Email" required="required" /></td>
-      <td><input type="text" name="usuario" placeholder="Nombre Usuario" required="required" /></td>
-        <td><input type="password" name="contrasena"  placeholder="Contraseña" required="required"/> </td>		 
-      <tr>  <td><input type="tel" name="telefono" placeholder="Telefono" />       </td>
-        <td><input type="tel" name="celular" placeholder="Telefono Celular" required="required" />      </td>
-        <td><input type="text" name="fax" placeholder="Fax" />     </td>   
-         <td><input type="text" name="direccion" placeholder="Direccion" required="required" />      </td></tr>  
-        <td><input type="submit" value="Agregar Cliente" class="boton"/>      </td>
-        <td></td>
-      
-        </form> 
-    </tr>
-</table>
+<div class="container">
+<div class="col-md-4">
+    <a href="registrarnuevocliente.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
+</div>
+<div class="col-md-4">
+        <a href="actualizacliente.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
+</div>
+<div class="col-md-4">
+        <a href="eliminarclienten.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
 
-
+</div>
+</div>
 <?php include("piedepagina.php");		?>

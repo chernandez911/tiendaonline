00ec04bbc28e73da8e72bdf68cfde5d380c105c6
chernandez911 <ing.cristianh@gmail.com>
2014-04-03 12:15:41 -0500

    Arreglos en el panel de administracion

--- admin/eliminarproducto.php
+++ admin/eliminarproducto.php
@@ -1,8 +1,12 @@
 <?php 
 include ("../config/config.php");
 	
-$id=$conn->QSTR($_GET['id'],get_magic_quotes_gpc());	
+$id=$conn->QSTR($_GET['id'],get_magic_quotes_gpc());
+
+$consulta=$conn->Execute("DELETE FROM imagenesproductos WHERE id=$id");	
 $consulta=$conn->Execute("DELETE FROM productos WHERE id=$id");	
+
+echo'<script type="text/javascript">alert("El producto fue eliminado de la base de datos");</script>';
 ?>
 
 <script type="text/javascript">

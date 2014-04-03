ea7f1b07d208d7523c5d8b57870401827a788f9f
chernandez911 <ing.cristianh@gmail.com>
2014-04-03 12:13:20 -0500

    Arreglos en el panel de administracion

---
+++ admin/eliminarproducton.php
@@ -0,0 +1,42 @@
+<?php include("cabecera.php");		?>
+<table>
+<?php 
+include ("../config/config.php");
+	echo "<tr><td>Nombre del producto</td>
+	<td>Precio</td>
+	<td>Medidas</td>
+	<td>Peso</td>
+	<td>Existencias</td>
+	<td>Estado</td></tr>";
+	
+$consulta=$conn->Execute("SELECT *from productos");
+while(!$consulta->EOF){	
+$estado_producto=$consulta->fields['estado_producto'] ;
+
+switch($estado_producto){
+	case 0:$digaestado= "No Activo"; break;
+	case 1:$digaestado= "Activo"; break;
+	}
+
+	echo'<tr>
+	<td>'.$consulta->fields['nombre'].'</td>
+	<td>'.$consulta->fields['precio'].'</td>
+	<td>Longitud:'.$consulta->fields['longitud'].''."<br>".'Anchra'.$consulta->fields['anchura'].''."<br>".'Altura'.$consulta->fields['altura'].
+	'<td>'.$consulta->fields['peso'].'</td><td>'.$consulta->fields['existencias'].'</td>
+	<td';
+	switch($estado_producto){
+	case 0:echo' style="background:rgb(255,200,200);"'; break;
+	case 1:echo' style="background:rgb(200,255,200);"'; break;
+	}
+	echo'>'.$digaestado.'</td>
+	<td><a href="eliminarproducto.php?id='.$consulta->fields['id'].'"><button>Eliminar Producto</button> </a></td>
+	</tr>';
+
+	$consulta->moveNext();
+}
+?>
+</table>
+
+
+
+<?php include("piedepagina.php");		?>

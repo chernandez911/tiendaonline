c4218bb3195bdd7869e8372866304dae5bf5f207
chernandez911 <ing.cristianh@gmail.com>
2014-04-03 12:16:22 -0500

    Arreglos en el panel de administracion

--- admin/piedepagina.php
+++ admin/piedepagina.php
@@ -1,10 +1,28 @@
-
-			</section>
-            <div style=" clear:both;"></div>
-			<footer>
-			<h6> (c) Ferretaller Hernandez Hermanos </h6>
-			</footer>
-		</div>
-	</body>
+</div>
+<div id="footer" class="fixed">
+	<div class="container">
+	    <div class="row-fluid">
+	      	<div class="col-md-4">
+	      		<br>
+	      		<a href=""> Mapa del sitio </a> <br>
+	      		<a href=""> Terminos y condiciones </a> <br>
+	      		<a href=""> Politica de privacidad </a> <br>     					
+	      	</div>
+	<div class="col-md-4">
+		<br>
+	    <a href=""> Mi cuenta </a> <br>
+	    <a href="admin/"> Administrador </a> <br>
+	</div>
+			<div class="col-md-4">
+				<p style="color:white">Siguenos en: </p> 
+			   	<ul id="marcadores">
+					<li><a class="facebook" href="#"  title="Fan Page en facebook"></a></li>
+					<li><a class="twitter" href="#" title="Seguir en Twitter"></a></li>
+					<li><a class="instagram" href="#" title="Seguir en Instagram"></a></li>
+				</ul>
+			</div>
+	    	</div>
+    </div>
+</div>  
+</body>
 </html>
-

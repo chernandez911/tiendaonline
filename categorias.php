01af1662618f609ff38d6db862de25f703575fea
chernandez911 <ing.cristianh@gmail.com>
2014-04-03 12:17:35 -0500

    Arreglos

--- categorias.php
+++ categorias.php
@@ -12,21 +12,25 @@ $consulta=$conn->Execute("SELECT *FROM productos WHERE id_categoria =$id && exis
 while (!$consulta->EOF){
 
 $consulta2=$conn->Execute("SELECT * FROM imagenesproductos WHERE id_producto='".$consulta->fields['id']." ' LIMIT 1;");
-
-  echo"<div id='productos' class='paneldestacados panel panel-info col-xs-12 col-sm-6 col-md-6 col-lg-6'> ";
-  echo" <div class='panel-heading'>".$consulta->fields['nombre']."</div> ";
-  echo"<img src='photo/".$consulta2->fields['imagen']."' width=100px class=".'img-rounded'."> "; 
+ $numero = $consulta->fields['precio']; 
+echo('<div id="productos" class="row col-xs-12 col-sm-6 col-md-4 col-lg-4">
+  <div class="paneldestacados col-xs-12 col-sm-6 col-md-4 col-lg-4">');
+  echo'
+    <div class="thumbnail">
+     <img src="photo/'.$consulta2->fields['imagen'].'" width=150px class="img-rounded">
+      <div class="caption">
+        <h3><a href="producto.php?id='.$consulta->fields['id'].'">'.$consulta->fields['nombre'].'</a></h3>
+        <p>Precio $ '.number_format($numero,0,",",".").' </p>
+        <p><a href="producto.php?id='.$consulta->fields['id'].'"><button class="btn btn-ifno"> Ver producto </button></a></p>';
+        echo"Cantidad:<input type='number' class='btn btn-default' value='1' max='10' min='1' id='numero".$consulta->fields['id']."'>";
+    echo('    <button value="'.$consulta->fields['id'].'"class="botoncompra btn btn-default"> Al carrito </button>
+      </div>
+    </div>
+  </div>
+</div>');
   $consulta2->moveNext();
 
-  echo"<div class='col-xs-6 col-sm-6 col-md-6  col-lg-6'>";
-  echo "<h3><a href='producto.php?id=".$consulta->fields['id']."'></a></h3>";
-  $numero = $consulta->fields['precio']; 
-  echo "<p>Precio \$ ".number_format($numero,0,",",".")." </p> ";
-  echo"Cantidad:<input type='number' class='btn btn-default' value='1' max='10' min='1' id='numero".$consulta->fields['id']."'>";  
-  echo "<a href='producto.php?id=".$consulta->fields['id']."'><button class='btn btn-ifno'> Mas informacion </button></a>";
-  echo "<button value='".$consulta->fields['id']."'class='botoncompra btn btn-default'> Comprar Ahora </button>";
-  echo"</div>";
-  echo"</div>";
+
 
   $consulta->moveNext();
 }

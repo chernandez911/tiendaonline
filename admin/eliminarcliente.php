7f2ef97fa2bb5e675f8c295f6e6875f0f71d3375
chernandez911 <ing.cristianh@gmail.com>
2014-04-03 12:13:06 -0500

    Arreglos en el panel de administracion

--- admin/eliminarcliente.php
+++ admin/eliminarcliente.php
@@ -1,10 +1,7 @@
 <?php 
 include ("../config/config.php");
-
-
 $id=$conn->QSTR($_GET['id'],get_magic_quotes_gpc());	
 $consulta=$conn->Execute("DELETE FROM clientes WHERE id=$id");	
-
 ?>
 
 <script type="text/javascript">

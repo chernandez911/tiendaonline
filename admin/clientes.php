39b212a7e9e41690423d16d5142bdc0c0b884f2b
chernandez911 <ing.cristianh@gmail.com>
2014-04-04 18:34:44 -0500

    Mejoras en diseño; Creando menu y subiendo imagenes para las diferentes opciones que tiene el administrador para tratamiento de los clientes

--- admin/clientes.php
+++ admin/clientes.php
@@ -2,13 +2,13 @@
 
 <div class="container">
 <div class="col-md-4">
-    <a href="registrarnuevocliente.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
+    <a href="registrarnuevocliente.php" > <img class="featurette-image img-responsive img-circle" src="../img/registrarusuarios.fw.png" alt="Generic placeholder image"> </a>
 </div>
 <div class="col-md-4">
-        <a href="actualizacliente.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
+        <a href="actualizacliente.php" > <img class="featurette-image img-responsive img-circle" src="../img/actualizarusuarios.fw.png" alt="Generic placeholder image"> </a>
 </div>
 <div class="col-md-4">
-        <a href="eliminarclienten.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
+        <a href="eliminarclienten.php" > <img class="featurette-image img-responsive" src="../img/eliminarcliente.fw.png" alt="Generic placeholder image"> </a>
 
 </div>
 </div>

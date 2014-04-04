8b892771178501a63a191fb7e4af0ebb157e10ae
chernandez911 <ing.cristianh@gmail.com>
2014-04-04 18:36:53 -0500

    Mejorando el diseño del pie de pagina del administrador

--- admin/piedepagina.php
+++ admin/piedepagina.php
@@ -10,16 +10,12 @@
 	      	</div>
 	<div class="col-md-4">
 		<br>
-	    <a href=""> Mi cuenta </a> <br>
-	    <a href="admin/"> Administrador </a> <br>
+	 
+	   <p style="color:white">Siguenos en: </p> 
 	</div>
 			<div class="col-md-4">
-				<p style="color:white">Siguenos en: </p> 
-			   	<ul id="marcadores">
-					<li><a class="facebook" href="#"  title="Fan Page en facebook"></a></li>
-					<li><a class="twitter" href="#" title="Seguir en Twitter"></a></li>
-					<li><a class="instagram" href="#" title="Seguir en Instagram"></a></li>
-				</ul>
+				 <a href="admin/"> Salir</a> <br>
+				
 			</div>
 	    	</div>
     </div>

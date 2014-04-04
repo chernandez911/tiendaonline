20be0a5beb1bc62d987b66cd294f5cdf33dd230f
chernandez911 <ing.cristianh@gmail.com>
2014-04-04 18:37:26 -0500

    Mejoras en diseño, implementando el menu de opciones del sistema

--- admin/productos.php
+++ admin/productos.php
@@ -1,13 +1,13 @@
 <?php include("cabecera.php");		?>
 <div class="container">
 <div class="col-md-4">
-    <a href="insertarproducto.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
+    <a href="insertarproducto.php" > <img class="featurette-image img-responsive img-circle" src="../img/ingresarproducto.fw.png" alt="Generic placeholder image"> </a>
 </div>
 <div class="col-md-4">
-        <a href="actualizaproducto.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
+        <a href="actualizaproducto.php" > <img class="featurette-image img-responsive img-circle" src="../img/actualizarproductos.fw.png" alt="Generic placeholder image"> </a>
 </div>
 <div class="col-md-4">
-        <a href="eliminarproducton.php" > <img class="featurette-image img-responsive img-circle" src="../img/tiendaonline.fw.png" alt="Generic placeholder image"> </a>
+        <a href="eliminarproducton.php" > <img class="featurette-image img-responsive img-circle" src="../img/eliminarproducto.fw.png" alt="Generic placeholder image"> </a>
 
 </div>
 </div>

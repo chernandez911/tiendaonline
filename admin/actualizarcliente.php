7ab1d7d7086696a1c6523c38394dce4f5844b77e
chernandez911 <ing.cristianh@gmail.com>
2014-04-04 18:31:30 -0500

    Mejoras en diseño

--- admin/actualizarcliente.php
+++ admin/actualizarcliente.php
@@ -1,29 +1,31 @@
-<?php include("cabecera.php");		?>
-<table border="1">
 <?php 
+include("cabecera.php");
 include ("../config/config.php");
-	
+
 $id=$conn->QSTR($_GET["id"],get_magic_quotes_gpc());
-
 $consulta=$conn->Execute("SELECT *from clientes WHERE id=$id");
-while(!$consulta->EOF){
 
-	echo'<tr>
+while(!$consulta->EOF)
+{
+echo '<div class="container">';
+echo '<div class="table-responsive">';
+echo '<table class="table ">';
+	echo'<tbody>
+	<tr>
 	<form action="actualizarclientes.php?id='.$consulta->fields['id'].'" method="post">
-	<td><input type="text" name="nombre" value="'.$consulta->fields['nombre'].'"</td>
-	<td><input type="text" name="apellidos" value="'.$consulta->fields['apellidos'].'"</td>
-	<td><input type="text" name="email" value="'.$consulta->fields['email'].'"
-	<td><input type="text" name="usuario" value="'.$consulta->fields['usuario'].'"
-	<td><input type="text" name="contrasena" value="'.$consulta->fields['contrasena'].'"</td>
-	<td><input type="tel" name="telefono" value="'.$consulta->fields['telefono'].'"</td>
-	<td><input type="tel" name="celular" value="'.$consulta->fields['celular'].'"</td>
-	<td><input type="tel" name="fax" value="'.$consulta->fields['fax'].'"</td>
-	<td><input type="text" name="direccion" value="'.$consulta->fields['direccion'].'"</td>
-	<td><input type="submit" value="Actualizar"></td>';
-
+	<tr> <td> Nombre</td> <td><input class="form-control" type="text" name="nombre" value="'.$consulta->fields['nombre'].'"</td></tr> <p style="font-size:32px; font-family:Aubrey;"> Datos del usuario </p>
+	<tr><td> Apellidos</td> <td><input class="form-control" type="text" name="apellidos" value="'.$consulta->fields['apellidos'].'"</td></tr> 	
+	<tr><td> E-mail</td> <td><input class="form-control" type="text" name="email" value="'.$consulta->fields['email'].'" </td></tr> 
+	<tr><td> Usuario</td> <td><input class="form-control" type="text" name="usuario" value="'.$consulta->fields['usuario'].'" </td></tr>
+	<tr><td> Contraseña</td> <td><input class="form-control" type="text" name="contrasena" value="'.$consulta->fields['contrasena'].'"</td></tr>
+	<tr><td> Telefono Fijo</td> <td><input class="form-control" type="tel" name="telefono" value="'.$consulta->fields['telefono'].'"</td></tr>
+	<tr><td> Celular</td> <td><input class="form-control" type="tel" name="celular" value="'.$consulta->fields['celular'].'"</td></tr>
+	<tr><td> Fax</td> <td><input class="form-control" type="tel" name="fax" value="'.$consulta->fields['fax'].'"</td></tr>
+	<tr><td> Direccion de entrega</td> <td><input class="form-control" type="text" name="direccion" value="'.$consulta->fields['direccion'].'"</td></tr></tbody>';
+    echo "</table> <input class='btn btn-succes' type='submit' value='Actualizar'>";
+   echo '</div>';
+   echo '</div>'; 
 $consulta->moveNext();
 }
-
 ?>
-</table>
 <?php include("piedepagina.php");		?>

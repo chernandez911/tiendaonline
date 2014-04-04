965bf0110b96c1ee039b8f29f3813f773566a918
chernandez911 <ing.cristianh@gmail.com>
2014-04-04 18:33:54 -0500

    Haciendo mejoras

--- admin/cancelarpedido.php
+++ admin/cancelarpedido.php
@@ -1,14 +1,16 @@
+<?php include("cabecera.php");		?>
 <?php 
 include ("../config/config.php");
+
+
+
 $id=$conn->QSTR($_GET["id"],get_magic_quotes_gpc());
 $consulta=$conn->Execute("UPDATE pedidos SET estado=2 WHERE id=$id");
 
 if($consulta){
-	echo"<script type='text/javascript'> alert('El pedido fue cancelado un correo fue enviado al comprador, para informarle lo sucedido'); 
-	</script>";
+
+	echo'<a class="warning" data-dismiss="alert">El pedido fue cancelado un correo fue enviado al comprador, para informarle lo sucedido</a>';
+	
 }
 ?>
 
-<script>
-window.location = "../admin/pedidos.php";
-</script>

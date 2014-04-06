a404e12aadf92b59c7491fa697315aca2d3dfe3d
chernandez911 <ing.cristianh@gmail.com>
2014-04-06 12:04:17 -0500

    Estructuracion del codigo

--- admin/eliminarclienten.php
+++ admin/eliminarclienten.php
@@ -1,32 +1,43 @@
-<?php include("cabecera.php");      ?>
 <?php 
+include("cabecera.php"); 
 include ("../config/config.php");
 $consulta=$conn->Execute("SELECT *from clientes");
-while(!$consulta->EOF){
 
 echo '<div class="container">';
-
-echo '<div class="table-responsive">';
-
-echo '<table class="table table-striped">';
-echo'<p style="font-size:32px; font-family:Aubrey;">Eliminar usuarios registrados</p>';
-
-echo '<thead><tr><td>Nombre</td><td>Apellidos</td><td>E-mail</td><td>Telefono</td><td>Celular</td><td>Fax</td><td>Direccion</td></thead>';
-	echo "<tbody><tr><td>".$consulta->fields['nombre']."</td>
-    <td>".$consulta->fields['apellidos']."</td>
-    <td>".$consulta->fields['email']."</td>
-    <td>".$consulta->fields['telefono']."</td>
-    <td>".$consulta->fields['celular']."</td>
-    <td>".$consulta->fields['fax']."</td>
-    <td>".$consulta->fields['direccion']."</td>
-    <td><a href='eliminarcliente.php?id=".$consulta->fields['id']."''><button>Eliminar Usuario</button> </a></td>
-    </tr></tbody>";
-
-    echo "</table>";
-   echo '</div>';
-   echo '</div>'; 
-  $consulta->moveNext();
+echo'   <p style="font-size:32px; font-family:Aubrey;">Eliminar usuarios registrados</p>';
+echo '</div>'; 
+while(!$consulta->EOF)
+{
+echo '<div class="container">';
+echo '  <div class="table-responsive">';
+echo '    <table class="table table-striped">';
+echo '    <thead>
+            <tr>
+            <td>Nombre</td>
+            <td>Apellidos</td>
+            <td>E-mail</td>
+            <td>Telefono</td>
+            <td>Celular</td>
+            <td>Fax</td>
+            <td>Direccion</td>
+            </tr>
+          </thead>';
+echo "    <tbody>
+            <tr>
+            <td>".$consulta->fields['nombre']."</td>
+            <td>".$consulta->fields['apellidos']."</td>
+            <td>".$consulta->fields['email']."</td>
+            <td>".$consulta->fields['telefono']."</td>
+            <td>".$consulta->fields['celular']."</td>
+            <td>".$consulta->fields['fax']."</td>
+            <td>".$consulta->fields['direccion']."</td>
+            <td><a href='eliminarcliente.php?id=".$consulta->fields['id']."''><button>Eliminar Usuario</button> </a></td>
+            </tr>
+          </tbody>";
+echo "    </table>";
+echo '  </div>';
+echo '</div>'; 
+$consulta->moveNext();
 }
-  
+include("piedepagina.php");
 ?>
-<?php include("piedepagina.php");       ?>

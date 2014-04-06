c7641ddfca7ffd3588e1673b24a474304d4aeb2c
chernandez911 <ing.cristianh@gmail.com>
2014-04-06 11:43:43 -0500

    Estructuracion del codigo para que se pueda entender mejor

--- admin/actualizacliente.php
+++ admin/actualizacliente.php
@@ -3,28 +3,41 @@ include("cabecera.php");
 include ("../config/config.php");
 
 $consulta=$conn->Execute("SELECT *from clientes");
-
-while(!$consulta->EOF)
-{
-
 echo '<div class="container">';
-echo '<div class="table-responsive">';
-echo '<table class="table table-striped">';
-echo'<p style="font-size:32px; font-family:Aubrey;"> Usuarios Registrados</p>';
-echo '<thead><tr><td>Nombre</td><td>Apellidos</td><td>E-mail</td><td>Telefono</td><td>Celular</td><td>Fax</td><td>Direccion</td></thead>';
-	echo "<tbody><tr><td>".$consulta->fields['nombre']."</td>
-    <td>".$consulta->fields['apellidos']."</td>
-    <td>".$consulta->fields['email']."</td>
-    <td>".$consulta->fields['telefono']."</td>
-    <td>".$consulta->fields['celular']."</td>
-    <td>".$consulta->fields['fax']."</td>
-    <td>".$consulta->fields['direccion']."</td>
-    <td><a href='actualizarcliente.php?id=".$consulta->fields['id']."''><button>Actualizar Cliente</button> </a></td>
-    </tr></tbody>";
-    echo "</table>";
-   echo '</div>';
-   echo '</div>';
-  $consulta->moveNext();
+echo '   <p style="font-size:32px; font-family:Aubrey;"> Usuarios Registrados</p>';
+echo '</div>';
+
+while(!$consulta->EOF){
+echo '<div class="container">';
+echo '  <div class="table-responsive">';
+echo '    <table class="table table-striped">';
+echo '      <thead>
+              <tr>
+                <td>Nombre</td>
+                <td>Apellidos</td>
+                <td>E-mail</td>
+                <td>Telefono</td>
+                <td>Celular</td>
+                <td>Fax</td>
+                <td>Direccion</td>
+              </tr>
+            </thead>';
+	echo "       <tbody>
+                 <tr>
+                    <td>".$consulta->fields['nombre']."</td>
+                    <td>".$consulta->fields['apellidos']."</td>
+                    <td>".$consulta->fields['email']."</td>
+                    <td>".$consulta->fields['telefono']."</td>
+                    <td>".$consulta->fields['celular']."</td>
+                    <td>".$consulta->fields['fax']."</td>
+                    <td>".$consulta->fields['direccion']."</td>
+                    <td><a href='actualizarcliente.php?id=".$consulta->fields['id']."''><button>Actualizar Cliente</button> </a></td>
+                  </tr>
+                </tbody>";
+echo "</table>";
+echo '</div>';
+echo '</div>';
+$consulta->moveNext();
 } 
+include("piedepagina.php");
 ?>
-<?php include("piedepagina.php");       ?>

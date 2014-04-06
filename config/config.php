b62176fa5cb79559e33a93bdce3c7c6cda5b478e
chernandez911 <ing.cristianh@gmail.com>
2014-04-06 12:19:11 -0500

    tratando de mejorar las relaciones relativas

--- config/config.php
+++ config/config.php
@@ -1,10 +1,9 @@
 <?php 
-include('adodb/adodb.inc.php');  
+include_once('/adodb/adodb.inc.php');  
 $server='localhost'; 
 $user='root'; 
 $pass=''; 
 $db='tiendaonline'; 
 $conn = &ADONewConnection('mysql'); 
 $conn->PConnect($server,$user,$pass,$db);
-
 ?>

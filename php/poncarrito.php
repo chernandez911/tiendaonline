438c294f35570ee569f12f87fc5db6d36c95eda4
chernandez911 <ing.cristianh@gmail.com>
2014-04-06 12:15:36 -0500

    ...

--- php/poncarrito.php
+++ php/poncarrito.php
@@ -1,25 +1,21 @@
-<?php include ("../config/config.php"); ?>
-<?php 
- session_start();
- $suma=0;
- if(isset($_GET['p'])){
-
+<?php  
+include ("../config/config.php");
+session_start();
+$suma=0;
+if(isset($_GET['p']))
+{
  $_SESSION['producto'][$_SESSION['contador']]=intval($_GET['p']);
  $_SESSION['unidades'][$_SESSION['contador']]=intval($_GET['cant']);
  $_SESSION['contador']++;
- }
-
+}
 for($i=0;$i<($_SESSION['contador']);$i++)
 {
 	$consulta=$conn->Execute("SELECT *FROM productos WHERE id=".intval($_SESSION['producto'][$i])."");
 		while(!$consulta->EOF)
 		{
-	
 			echo("<table class='table'>");
 			echo "<tr><td>". $_SESSION['unidades'][$i]."</td><td>".$consulta->fields['nombre']."</td><td>\$ ". number_format(($_SESSION['unidades'][$i]*$consulta->fields['precio']),0,",",".")."</td></tr>";
-			$suma+=$consulta->fields['precio'] *  $_SESSION['unidades'][$i];
-
-			
+			$suma+=$consulta->fields['precio'] *  $_SESSION['unidades'][$i];			
 			$consulta->moveNext();
 		}
 }

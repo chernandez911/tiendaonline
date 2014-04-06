049b2c265b7e16ba547a671b53bbd09f3af28c30
chernandez911 <ing.cristianh@gmail.com>
2014-04-06 12:10:42 -0500

    Cambio en el diseño en los botones y en la consulta para que muestre mas datos

--- admin/pedidos.php
+++ admin/pedidos.php
@@ -1,35 +1,41 @@
-<?php include("cabecera.php");		?>
-<table>
 <?php 
-include ("../config/config.php");
+error_reporting(E_ALL  and ~ E_NOTICE  and ~ E_DEPRECATED) ;
+
+include("cabecera.php");	
+include ("../config/config.php");	
+echo'<div class="container">
+		<p style="font-size:32px; font-family:Aubrey;">Pedidos</p>
+	</div>';
 	
-$consulta=$conn->Execute("SELECT pedidos.id AS id_pedido,fecha,estado,nombre,apellidos FROM pedidos LEFT JOIN clientes ON pedidos.id_cliente=clientes.id ORDER BY estado,fecha ASC");
+$consulta=$conn->Execute("SELECT pedidos.id AS id_pedido,fecha,estado,nombre,apellidos,email,celular FROM pedidos LEFT JOIN clientes ON pedidos.id_cliente=clientes.id ORDER BY estado,fecha ASC");
 while(!$consulta->EOF){
-
+echo '<div class="container">';
+echo '	<div class="table-responsive">';
+echo '		<table class="table">';
 $estadopedido=$consulta->fields['estado'] ;
-
-//if ($estadopedido==0){ $digaestado="No entregado";}else{$digaestado="Entregado";}
-switch($estadopedido){
+switch($estadopedido)
+{
 	case 0:$digaestado = "No Entregado"; break;
 	case 1:$digaestado = "Procesando"; break;
 	case 2:$digaestado = "Cancelado"; break;
-	}
-	echo'<tr';
-	//if ($estadopedido==0){echo' style="background:rgb(255,200,200);"';} else {echo' style="background:rgb(200,255,200);"';}
-	switch($estadopedido){
+}
+echo'<tr';
+switch($estadopedido)
+{
 	case 0:echo' style="background:rgb(255,200,200);"'; break;
 	case 1:echo' style="background:rgb(200,255,200);"'; break;
 	case 2:echo' style="background:rgb(255,255,200);"'; break;
-	}
-	echo'>
-		<td>'.$consulta->fields['nombre']." ".$consulta->fields['apellidos'].'</td>
+}
+echo'>
+		<td>'.$consulta->fields['nombre']." ".$consulta->fields['apellidos'].''.$consulta->fields['email'].''.$consulta->fields['celular'].'</td>
 		<td>'.date("M d Y H:i:s",$consulta->fields['fecha']).'</td>
 		<td>'.$digaestado.'</td>
-		<td><a href="gestionpedidos.php?id='.$consulta->fields['id_pedido'].'"><button>Gestionar</button></a></td>
-		<td><a href="pedidosentregados.php?id='.$consulta->fields['id_pedido'].'"><button>Producto Entregado</button></a></td>
-		<td><a href="cancelarpedido.php?id='.$consulta->fields['id_pedido'].'"><button>Cancelar Pedido</button></a></td></tr>';
+		<td><a href="gestionpedidos.php?id='.$consulta->fields['id_pedido'].'"><button class="btn btn-info">Gestionar</button></a></td>
+		<td><a href="pedidosentregados.php?id='.$consulta->fields['id_pedido'].'"><button class="btn btn-success">Producto Entregado</button></a></td>
+		<td><a href="cancelarpedido.php?id='.$consulta->fields['id_pedido'].'"><button class="btn btn-danger">Cancelar Pedido</button></a></td></tr>';
+		echo"</table>";
+	echo"</div></div>";
 $consulta->moveNext();
 }
+include("piedepagina.php");
 ?>
-</table>
-<?php include("piedepagina.php");		?>

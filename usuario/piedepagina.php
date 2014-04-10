02ba71c8033d3c9456d886d1e00c30ac7ed26900
chernandez911 <ing.cristianh@gmail.com>
2014-04-10 18:09:58 -0500

    Piedepagina para el panel del cliente

---
+++ usuario/piedepagina.php
@@ -0,0 +1,24 @@
+</div>
+<div id="footer" class="fixed">
+	<div class="container">
+	    <div class="row-fluid">
+	      	<div class="col-md-4">
+	      		<br>
+	      		<a href=""> Mapa del sitio </a> <br>
+	      		<a href=""> Terminos y condiciones </a> <br>
+	      		<a href=""> Politica de privacidad </a> <br>     					
+	      	</div>
+	<div class="col-md-4">
+		<br>
+	 
+	   <p style="color:white">Siguenos en: </p> 
+	</div>
+			<div class="col-md-4">
+				 <a href="admin/"> Salir</a> <br>
+				
+			</div>
+	    	</div>
+    </div>
+</div>  
+</body>
+</html>

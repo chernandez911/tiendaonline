9747951c8d08d67cccdb9856038bd340b5968c4a
chernandez911 <ing.cristianh@gmail.com>
2014-04-14 18:40:03 -0500

    Agregado el codigo de verificacion de variable de sesion, eliminando el mensaje de alerta para ser cambiado por la clase mail de php para que automaticamente envie un mensaje al correo del comprador diciendo que su pedido fue cancelado por el motivo que el administrador escoja entre una select

--- admin/cancelarpedido.php
+++ admin/cancelarpedido.php
@@ -1,15 +1,17 @@
-<?php include("cabecera.php");		?>
 <?php 
+session_start();
+if(!isset($_SESSION['usuario'])) 
+{
+  header('Location: login.php'); 
+  exit();
+}
+include("cabecera.php");
 include ("../config/config.php");
-
-
 
 $id=$conn->QSTR($_GET["id"],get_magic_quotes_gpc());
 $consulta=$conn->Execute("UPDATE pedidos SET estado=2 WHERE id=$id");
 
 if($consulta){
-
-	echo'<a class="warning" data-dismiss="alert">El pedido fue cancelado un correo fue enviado al comprador, para informarle lo sucedido</a>';
 	
 }
 ?>

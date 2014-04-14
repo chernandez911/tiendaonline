974eea75691def57443e3fd0aedbb850ab8e74ec
chernandez911 <ing.cristianh@gmail.com>
2014-04-14 18:27:28 -0500

    Mejora en diseño se agrego un contenedor para darle referencia al administrador que esta viendo los productos registrados, verificacion de la variable de sesion

--- admin/actualizaproducto.php
+++ admin/actualizaproducto.php
@@ -1,13 +1,24 @@
 <?php 
-include("cabecera.php"); 
+session_start();
+if(!isset($_SESSION['usuario'])) 
+{
+  header('Location: login.php'); 
+  exit();
+}
+?>
+<?php include("cabecera.php"); ?>
+<div class="container">
+<p style="font-size:32px; font-family:Aubrey;"> Productos Registrados</p>
+</div>
+<?php
 include ("../config/config.php");
-	
 $consulta=$conn->Execute("SELECT *from productos");
 while(!$consulta->EOF){	
 
 $estado_producto=$consulta->fields['estado_producto'] ;
 
-switch($estado_producto){
+switch($estado_producto)
+{
 	case 0:$digaestado= "No Activo"; break;
 	case 1:$digaestado= "Activo"; break;
 }
@@ -15,27 +26,28 @@ echo '<div class="container">';
 echo '	<div class="table-responsive">';
 echo '		<table class="table table-striped">';
 echo "			<tr>
-					<td>Nombre del producto</td>
-					<td>Precio</td>
-					<td>Medidas</td>
-					<td>Peso</td>
-					<td>Existencias</td>
-					<td>Estado</td>
-					<td>Opcion</td>
+				<td>Nombre del producto</td>
+				<td>Precio</td>
+				<td>Medidas</td>
+				<td>Peso</td>
+				<td>Existencias</td>
+				<td>Estado</td>
+				<td>Opcion</td>
 				</tr>";
-echo'			<tr>
-					<td>'.$consulta->fields['nombre'].'</td>
-					<td>'.$consulta->fields['precio'].'</td>
-					<td>Longitud:'.$consulta->fields['longitud'].''."<br>".'Anchra'.$consulta->fields['anchura'].''."<br>".'Altura'.$consulta->fields['altura'].
-					'<td>'.$consulta->fields['peso'].'</td><td>'.$consulta->fields['existencias'].'</td>
-					<td';
+echo'				<tr>
+						<td>'.$consulta->fields['nombre'].'</td>
+						<td>'.$consulta->fields['precio'].'</td>
+						<td>Longitud:'.$consulta->fields['longitud'].''."<br>".'Anchra: '.$consulta->fields['anchura'].''."<br>".'Altura: '.$consulta->fields['altura'].
+						'<td>'.$consulta->fields['peso'].'</td><td>'.$consulta->fields['existencias'].'</td>
+						<td';
 switch($estado_producto){
 	case 0:echo' style="background:rgb(255,200,200);"'; break;
 	case 1:echo' style="background:rgb(200,255,200);"'; break;
 }
-	echo'>'.$digaestado.'</td>
-					<td><a href="actualizarproducto.php?id='.$consulta->fields['id'].'"><button>Actualizar Producto</button> </a></td>
-				</tr>';
+echo'					>'.$digaestado.'</td>
+						<td><a href="actualizarproducto.php?id='.$consulta->fields['id'].'"><button>Actualizar Producto</button> </a> <br>
+			
+					</tr>';
 echo "		</table>";
 echo '	</div>';
 echo '</div>';

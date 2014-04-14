833dc2ceaaf3bc91608cb538222139335a34b011
chernandez911 <ing.cristianh@gmail.com>
2014-04-14 18:46:31 -0500

    Agregado el codigo de verificacion de variable de sesion

--- admin/eliminarproducto.php
+++ admin/eliminarproducto.php
@@ -1,4 +1,11 @@
 <?php 
+session_start();
+if(!isset($_SESSION['usuario'])) 
+{
+  header('Location: login.php'); 
+  exit();
+}
+
 include ("../config/config.php");
 	
 $id=$conn->QSTR($_GET['id'],get_magic_quotes_gpc());
@@ -8,7 +15,6 @@ $consulta=$conn->Execute("DELETE FROM productos WHERE id=$id");
 
 echo'<script type="text/javascript">alert("El producto fue eliminado de la base de datos");</script>';
 ?>
-
 <script type="text/javascript">
 window.location="productos.php";
 </script>

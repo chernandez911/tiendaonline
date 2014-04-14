1ce009dca98660adb8cd16f5881c4d099bfedf99
chernandez911 <ing.cristianh@gmail.com>
2014-04-14 18:56:32 -0500

    Estructuracion del codigo

--- admin/login.php
+++ admin/login.php
@@ -4,20 +4,15 @@
 	<meta charset="UTF-8">
 	<title>Login Tienda Online</title>
   <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-   <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/login.css">
-
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="../css/login.css">
 </head>
-<body>
-	
+<body>	
 <div id="head" class="container">
-
 <img class="img-responsive" src="../img/logoferretaller.png"/>
-
 </div>
 
 <div class="container">
-	
 <form class="form-signin" role="form" action="logeo.php" method="post">
 <p>	Inicio de Sesion </p> 
     <h2 class="form-signin-heading"></h2>
@@ -25,8 +20,8 @@
     <input class="form-control" name="pass" type="password" required="required" placeholder="Contraseña"></input>
    <br>
     <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesión</button>
-
 </form>
 </div>
+
 </body>
 </html>

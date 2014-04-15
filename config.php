35fa1be1507adb31906ef79478dab376b4ec6a43
chernandez911 <ing.cristianh@gmail.com>
2014-04-14 19:18:29 -0500

    Este archivo config sirve para que index2.php muestre los productos y para que no se genere conflicto llamando al adodb, ya esta probado y funciona

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+include("adodb/adodb.inc.php");  
+$server='localhost'; 
+$user='root'; 
+$pass=''; 
+$db='tiendaonline'; 
+$conn = &ADONewConnection('mysql'); 
+$conn->PConnect($server,$user,$pass,$db);
+?>

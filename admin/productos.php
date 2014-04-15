1e94622d54a68f4445277d1be2de2a40aed90366
chernandez911 <ing.cristianh@gmail.com>
2014-04-14 19:03:42 -0500

    Agregado el codigo de verificacion de variable de sesion, se agrego un item mas al menu donde el administrador puede agregar las imagenes que quiera al producto que escoja

--- admin/productos.php
+++ admin/productos.php
@@ -1,4 +1,12 @@
-<?php include("cabecera.php");		?>
+<?php 
+session_start();
+if(!isset($_SESSION['usuario'])) 
+{
+  header('Location: login.php'); 
+  exit();
+}
+include("cabecera.php");
+?>
 <div class="container">
 <div class="col-md-4">
     <a href="insertarproducto.php" > <img class="featurette-image img-responsive img-circle" src="../img/ingresarproducto.fw.png" alt="Generic placeholder image"> </a>
@@ -8,6 +16,22 @@
 </div>
 <div class="col-md-4">
         <a href="eliminarproducton.php" > <img class="featurette-image img-responsive img-circle" src="../img/eliminarproducto.fw.png" alt="Generic placeholder image"> </a>
+
+</div>
+</div>
+
+<div class="container">
+<div class="col-md-4">
+   
+</div>
+<div class="col-md-4">
+        <a href="insertaimagen.php"><img class="featurette-image img-responsive img-circle" src="../img/insertarimagen.fw.png" alt="Insertar Imagenes a los productos"> </a>
+</div>
+<div class="col-md-4">
+       
+
+</div>
+</div>
 
 </div>
 </div>

abf0bb5cc3554a2681c78cb6c863ff6e95ef0eb6
chernandez911 <ing.cristianh@gmail.com>
2014-04-14 19:00:01 -0500

    Destruir la session y el arreglo con todas las variables de sesion

--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,6 @@
-<?php
-session_start();
-session_destroy();
+<?php 
+ session_start();
+ $_SESSION = array();
+ session_destroy();
+ header("location:../index.php");
 ?>
-<script>window.location = "../index.php";</script>
